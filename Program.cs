a8004f61259fe1e7adfbc462fd7085ae816c881c
EriikJG <erik.gaibor@epn.edu.ec>
2022-02-28 17:42:36 -0500

    Revert "Merge branch 'Rama-Erik' into Rama-Nicole"
    This reverts commit 6206541ffe0c26240a7273a2b57b88d5a5222bf7, reversing
    changes made to 228c6a882b10b0e4431a0c64693ca8f1b69835f7.

--- Program.cs
+++ Program.cs
@@ -8,33 +8,6 @@ namespace ProyectGR3
 {
     internal static class Program
     {
-        public struct Direccion
-        {
-            public string calleP;
-            public string calleS;
-            public string Ncasa;
-        }
-        public struct Producto
-        {
-            public string nombreProducto;
-            public string talla;
-            public string color;
-            public string categoria;
-            public double precio;
-        }
-        public struct Pago
-        {
-            public string numeroTarjeta;
-            public int clave;
-        }
-
-            public struct Persona
-        {
-            public string nombre;
-            public string correo;
-            public Direccion datosD;
-            public Pago tarjeta;
-        }
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
@@ -45,6 +18,5 @@ namespace ProyectGR3
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new RopaDeNino());
         }
-
     }
 }
