228c6a882b10b0e4431a0c64693ca8f1b69835f7
EriikJG <erik.gaibor@epn.edu.ec>
2022-02-28 00:57:36 -0500

    Revert "Merge branch 'master' into Rama-Nicole"
    This reverts commit 9cd4f8158fb81c0050151b1c8a7303508bdfdf86, reversing
    changes made to 7a6dc5f98cbcd2fbe21118b9f9231791aee0bb5f.

--- RopaDeNino.cs
+++ RopaDeNino.cs
@@ -10,25 +10,11 @@ using System.Windows.Forms;
 
 namespace ProyectGR3
 {
-<<<<<<< HEAD:RopaDeNino.cs
     public partial class RopaDeNino : Form
     {
         public RopaDeNino()
         {
             InitializeComponent();
         }
-=======
-    public partial class VentanaFacturacion : Form
-    {
-        public VentanaFacturacion()
-        {
-            InitializeComponent();
-        }
-
-        private void VentanaFacturacion_Load(object sender, EventArgs e)
-        {
-
-        }
->>>>>>> master:VentanaFacturacion.cs
     }
 }
